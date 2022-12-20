cfa1c94602fc470ad9f6ea065bad43be4f47c45b
Tomer Revah <revahtomer@gmail.com>
2022-12-20 10:59:31 +0200

    show more and show less - task 1d

--- client/src/components/ticketPreview.tsx
+++ client/src/components/ticketPreview.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef, useEffect, useState } from 'react';
 import { Ticket } from '../api';
 
 const TicketPreview = ({
@@ -10,16 +10,37 @@ const TicketPreview = ({
   onHide: Function;
   hideTicketsIds: string[];
 }) => {
+  const ref = useRef<HTMLParagraphElement>(null);
+  const [isMoreThan3Lines, setIsMoreThan3Lines] = useState(false);
+  const [toggleShow, setToggleShow] = useState(false);
+
+  useEffect(() => {
+    if (ref.current && ref.current.clientHeight < ref.current.scrollHeight) {
+      setIsMoreThan3Lines(true);
+    }
+  }, [ref]);
+
   const style = hideTicketsIds.includes(ticket.id)
     ? { display: 'none' }
     : { display: 'block' };
+
   return (
     <li key={ticket.id} className='ticket' style={style}>
       <h5 className='title'>{ticket.title}</h5>
       <button className='hide-ticket' onClick={() => onHide(ticket.id)}>
         Hide
       </button>
-      <p>{ticket.content}</p>
+      <p
+        ref={ref}
+        className={`content ${toggleShow ? 'show-less' : 'show-more'}`}
+      >
+        {ticket.content}
+      </p>
+      {isMoreThan3Lines && (
+        <span onClick={() => setToggleShow(!toggleShow)}>
+          {toggleShow ? 'Show Less' : 'Show More'}
+        </span>
+      )}
       {ticket?.labels && (
         <div className='labels'>
           {ticket?.labels.map((label, index) => (
@@ -31,7 +52,7 @@ const TicketPreview = ({
       )}
       <footer>
         <div className='meta-data'>
-          By {ticket.userEmail} |{' '}
+          By {ticket.userEmail} |
           {new Date(ticket.creationTime).toLocaleString()}
         </div>
       </footer>
